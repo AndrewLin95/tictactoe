ed6dcbc6e3b80aa0686084eac39b6807b0aa9b52
AndrewLin95 <andrew.lin88@hotmail.com>
2022-07-04 18:44:39 -0400

    added gameflow functionality and ability to click on cell grids

--- javascript.js
+++ javascript.js
@@ -3,6 +3,7 @@ const gameBoard = (() => {
         const playGrid = document.querySelector('#playGrid');
     
         let iRow = 0;
+        let arrayNum = 0;
         while (iRow < 3){
             const row = document.createElement('div');
             row.className = 'rows';
@@ -11,16 +12,56 @@ const gameBoard = (() => {
             let iColumn = 0;
             while (iColumn < 3){
                 const column = document.createElement('div');
-                column.className = `columns ${iRow}_${iColumn}`;
+                column.className = `columns ${arrayNum}`;
+                column.id = arrayNum;
                 row.appendChild(column);
-                //column.addEventListener('click', )         To add eventlistner and function for when these divs are clicked
+                column.addEventListener('mousedown', testFunction);         
+                arrayNum++;
                 iColumn++;
             }
         iRow++;
+        };
+    };
+    const boardState = [                // will use this array to check the board state and declare game state
+        '', '', '',
+        '', '', '',
+        '', '', ''
+    ];
+
+    return {generateGrid, boardState};
+})();
+
+const testFunction = () => {
+    console.log('test');
+}
+
+// dictate player turn order and assigning either a O or X based on turn order. follows a simple toggle
+const gameFlow = (() => {
+    let i = 0;
+    const turnOrder = () => {
+        let turnText = document.querySelector('#turnTextBox');
+        if (i % 2 === 0){
+            turnText.textContent = `${playerOne}'s Turn!`;
+        } else{
+            turnText.textContent = `${playerTwo}'s Turn!`;
         }
+        i++
     }
-    return {generateGrid};
+
+    const playerAction = (num) => {
+        if (i % 2 === 0){
+            console.log(num);
+        }
+        turnOrder();
+    }
+    console.log(i);
+    return {turnOrder, playerAction};
 })();
 
 
+const playerOne = 'Player One';
+const playerTwo = 'Player Two';
+
 gameBoard.generateGrid();
+gameFlow.turnOrder();
+console.log(gameBoard.boardState);
